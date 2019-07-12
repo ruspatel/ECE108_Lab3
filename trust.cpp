2cd7b4c9938ed359e1ee9dd1c7472a6e6fff3307
Manbir Arora <manbir1125@gmail.com>
2019-07-11 22:31:04 -0400

    done printing equal array expression

--- trust.cpp
+++ trust.cpp
@@ -6,21 +6,23 @@ int main(){
     std::cin >> size;
     bool** adj_array = get_adjacency_matrix(size);
 
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
-            std::cout << adj_array[i][j] << " ";
-        }
-        std::cout<< std::endl;
-    }
+    // for(int i = 0; i < size; i++){
+    //     for(int j = 0; j < size; j++){
+    //         std::cout << adj_array[i][j] << " ";
+    //     }
+    //     std::cout<< std::endl;
+    // }
     bool** ref_array = get_reflexive_matrix(size, adj_array);
     std::cout <<std::endl;
 
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
-            std::cout << ref_array[i][j] << " ";
-        }
-        std::cout<< std::endl;
-    }
+    // for(int i = 0; i < size; i++){
+    //     for(int j = 0; j < size; j++){
+    //         std::cout << ref_array[i][j] << " ";
+    //     }
+    //     std::cout<< std::endl;
+    // }
+
+    print_equivalency(size,ref_array);
     return 0;
 }
 
@@ -71,6 +73,68 @@ bool** get_reflexive_matrix(int size_input, bool** array){
     }  
 
     return return_array;
+}
+
+void print_equivalency(int size_input, bool** array){
+    int size = size_input;
+    bool** reflex_array = array;
+
+    int num_array[size][size];
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j<size; j++){
+            if(reflex_array[i][j] == true){
+                num_array[i][j] = 1;
+            }else{
+                 num_array[i][j] = 0;
+            }
+        }
+        
+    }
+
+    for(int t = 0; t < size; t++){
+        bool equal = true;
+        bool t_good = true;
+
+        while(num_array[t][0] == -1){
+            if(t != size - 1){
+                t++;
+            }else{
+                t_good = false;
+                break;
+            }
+        }
+        if(t_good == false){
+            break;
+        }
+        std::cout << "{" << t + 1;
+        for(int y = t + 1; y < size; y++){
+            bool y_good = true;
+            while(num_array[y][0] == -1){
+                if(y != size - 1){
+                    y++;
+                }else{
+                    y_good = false;
+                    break;
+                }
+            }
+            if(y_good == false){
+                break;
+            }
+            for(int u = 0; u < size; u++){
+                equal = true;
+                if(num_array[t][u] != num_array[y][u]){
+                    equal = false;
+                    break;
+                }
+            }
+            if(equal != false){
+                std::cout << "," << y + 1;
+                num_array[y][0]  = -1;
+            }
+        }
+        std::cout << "}" <<std::endl;
+        num_array[t][0]  = -1;
+    }
 }
 
 
--- trust.h
+++ trust.h
@@ -1,2 +1,3 @@
 bool** get_adjacency_matrix(int size_input);
 bool** get_reflexive_matrix(int size_input, bool** array);
+void print_equivalency(int size_input, bool** array);
